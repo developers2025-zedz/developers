ad8c814089449953485f0fbe07ea39f65f1421df
zarif.bekturdiev <zarif.bekturdiev@dteam.dev>
2025-04-24 14:07:39 +0500

    Initialize Fastapi Project

---
+++ app/main.py
@@ -0,0 +1,6 @@
+import uvicorn
+from fastapi import FastAPI
+from app.api.v1.hello import router as hello_router
+
+app = FastAPI(title="FastAPI Docker Example")
+app.include_router(hello_router, prefix="/api/v1")
