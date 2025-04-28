f664cd2d35c394b9d602393336866b5098230076
zarif.bekturdiev <zarif.bekturdiev@dteam.dev>
2025-04-28 12:04:05 +0500

    Dockerized the project

--- app/api/v1/hello.py
+++ app/api/v1/hello.py
@@ -3,11 +3,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/")
-async def ok():
-    return {"message": "ok"}
-
-
 @router.get("/hello")
 async def say_hello():
     return {"message": "Hello, world!"}
--- app/main.py
+++ app/main.py
@@ -2,5 +2,5 @@ import uvicorn
 from fastapi import FastAPI
 from app.api.v1.hello import router as hello_router
 
-app = FastAPI(title="FastAPI Docker Example")
+app = FastAPI(title="FastAPI Docker Example", docs_url='/')
 app.include_router(hello_router, prefix="/api/v1")
