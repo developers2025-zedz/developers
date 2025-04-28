3769d5197759f1c6effd1f0ddd983c59338925cf
zarif.bekturdiev <zarif.bekturdiev@dteam.dev>
2025-04-28 13:09:58 +0500

    test endpoint

--- app/api/v1/hello.py
+++ app/api/v1/hello.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
+class UserData(BaseModel):
+    name: str
+    phone: str
 
 
 @router.get("/hello")
 async def say_hello():
     return {"message": "Hello, world!"}
+
+
+@router.post("/create_user")
+async def create_user(data: UserData):
+    return data
